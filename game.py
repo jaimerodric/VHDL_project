fd8a8c95fffca8931abf12fb71da5807704fa68e
Jaime <jaimercarave@gmail.com>
2020-05-26 23:57:23 +0200

    botones_posicion_mal

--- game.py
+++ game.py
@@ -43,32 +43,39 @@ SCREEN_SIZE       = (200, 60)
 def init_screen():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_SIZE))
-    #surface=pygame.Surface((VGA_SIZE))
     
-    n_player=pygame.image.load('Normal.png').convert_alpha()
-    a_player=pygame.image.load('Ataque.png').convert_alpha()
-    d_player=pygame.image.load('Defensa.png').convert_alpha()
+    n_player1=pygame.image.load('Normal.png').convert_alpha()
+    a_player1=pygame.image.load('Ataque.png').convert_alpha()
+    d_player1=pygame.image.load('Defensa.png').convert_alpha()
 
-    images=[n_player,a_player,d_player]
+    images1=[n_player1,a_player1,d_player1]
+
+    n_player2=pygame.transform.flip(n_player1,True,False)
+    a_player2=pygame.transform.flip(a_player1,True,False)
+    d_player2=pygame.transform.flip(d_player1,True,False)
+
+
+    images2=[n_player2,a_player2,d_player2]
     
 
     
    #surface=pygame.transform.scale(pygame.image.load('background.jpg').convert(),(VGA_SIZE))
    # screen.blit(surface,(0,0))
-    return screen,images
+    return screen,images1,images2
 
 @cocotb.coroutine
-def update_screen(dut, screen,images): #(x, y, color):
+def update_screen(dut, screen,images1,images2): #(x, y, color):
     while True:
         yield RisingEdge(dut.clk)
         screen.fill((255,255,255))
-        print(type(dut.estado_lucha))
-        if dut.estado_lucha =='00':
-            screen.blit(images[0], (0,0))
-        if dut.estado_lucha == '01':
-            screen.blit(images[1], (0,0))
-        if dut.estado_lucha == '10':
-            screen.blit(images[2], (0,0))        
+        #pos1=dut.pos1.value.integer
+        if dut.estado_luchad1 ==0b00:
+            screen.blit(images1[0], (0,0))
+        if dut.estado_luchad1 == 0b01:
+            screen.blit(images1[1], (0,0))
+        if dut.estado_luchad1 == 0b10:
+            screen.blit(images1[2], (0,0))
+            
         
 
         pygame.display.flip()
@@ -86,46 +93,69 @@ def check_for_events(dut):
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     dut._log.info("UP key pressed")
-                    dut.up = 1
+                    dut.UP = 1
                 elif event.key == pygame.K_DOWN:
                     dut._log.info("DOWN key pressed")
-                    dut.down = 1
-               # elif event.key == pygame.K_LEFT:
-               #     dut._log.info("LEFT key pressed")
-               #     dut.button_left = 1
-               # elif event.key == pygame.K_RIGHT:
-               #     dut._log.info("RIGHT key pressed")
-               #     dut.button_right = 1
+                    dut.DOWN = 1
+                elif event.key == pygame.K_LEFT:
+                    dut._log.info("LEFT key pressed")
+                    dut.LEFT = 1
+                elif event.key == pygame.K_RIGHT:
+                    dut._log.info("RIGHT key pressed")
+                    dut.RIGHT = 1
+                if event.key == pygame.K_w:
+                    dut._log.info("W key pressed")
+                    dut.W = 1
+                elif event.key == pygame.K_s:
+                    dut._log.info("S key pressed")
+                    dut.S = 1
+                elif event.key == pygame.K_a:
+                    dut._log.info("A key pressed")
+                    dut.A = 1
+                elif event.key == pygame.K_d:
+                    dut._log.info("D key pressed")
+                    dut.D = 1
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_UP:
                     dut._log.info("UP key released")
-                    dut.up = 0
+                    dut.UP = 0
                 elif event.key == pygame.K_DOWN:
                     dut._log.info("DOWN key released")
-                    dut.down = 0
-               # elif event.key == pygame.K_LEFT:
-               #     dut._log.info("LEFT key released")
-               #     dut.button_left = 0
-               # elif event.key == pygame.K_RIGHT:
-               #     dut._log.info("RIGHT key released")
-               #     dut.button_right = 0
+                    dut.DOWN = 0
+                elif event.key == pygame.K_LEFT:
+                    dut._log.info("LEFT key released")
+                    dut.LEFT = 0
+                elif event.key == pygame.K_RIGHT:
+                    dut._log.info("RIGHT key released")
+                    dut.RIGHT = 0
+                if event.key == pygame.K_w:
+                    dut._log.info("W key released")
+                    dut.W = 0
+                elif event.key == pygame.K_s:
+                    dut._log.info("S key released")
+                    dut.S = 0
+                elif event.key == pygame.K_a:
+                    dut._log.info("A key released")
+                    dut.A = 0
+                elif event.key == pygame.K_d:
+                    dut._log.info("D key released")
+                    dut.D = 0
 
 @cocotb.test()
 def test(dut):
     dut._log.info("Running test")
 
     # All buttons begin non-pressed (avoid 'U' values)
-    dut.up = 0
-    dut.down = 0
-   # dut.button_left = 0
-   # dut.button_right = 0
+    dut.UP = 0
+    dut.DOWN = 0
+    dut.LEFT = 0
+    dut.RIGHT = 0
 
     # Init screen
-    screen,images = init_screen() 
+    screen,images1,images2 = init_screen() 
  
-
     # Fork coroutine that continuosly updates the screen
-    cocotb.fork(update_screen(dut, screen,images))
+    cocotb.fork(update_screen(dut, screen,images1,images2))
 
     # Fork coroutine that continuosly checks for button presses
     cocotb.fork(check_for_events(dut))
@@ -136,9 +166,9 @@ def test(dut):
     cocotb.fork(c.start())
 
     # Assert and deassert reset
-    dut.rst = 1
+    dut.reset = 1
     yield Timer(RESET_DURATION, units='ns')
-    dut.rst = 0
+    dut.reset = 0
 
     # Wait until desired time
     dut._log.info("Entering for loop")
