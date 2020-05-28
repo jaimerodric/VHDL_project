bf27a66dec855b7d2f0ae0761da27106fac9eb4d
joseangelmp97 <65900218+joseangelmp97@users.noreply.github.com>
2020-05-28 20:18:10 +0200

    Add files via upload

--- estado_juego.vhd
+++ estado_juego.vhd
@@ -4,37 +4,70 @@ use IEEE.NUMERIC_STD.ALL;
 
 
 entity estado_juego is
-    Port ( --clk : in  STD_LOGIC;
-           --reset : in  STD_LOGIC;
-           pos2 : in  integer;
-           pos1 : in  integer;
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           pos2 : in  STD_LOGIC_VECTOR (9 downto 0);
+           pos1 : in  STD_LOGIC_VECTOR (9 downto 0);
            estado_luchador1 : in  STD_LOGIC_VECTOR (1 downto 0);
            estado_luchador2 : in  STD_LOGIC_VECTOR (1 downto 0);
+			  --vida1 : out std_logic_vector (3 downto 0);
+			  vida1 : out integer:= 3;
+			  vida2 : out integer:= 3;
+			  --vida2 : out std_logic_vector (3 downto 0);
            fin_juego : out  STD_LOGIC);
 end estado_juego;
 
 architecture Behavioral of estado_juego is
 
-signal dist : integer;
+signal dist : std_logic_vector(9 downto 0);
+signal corazones1,corazones2 : integer := 3;
+signal fin : std_logic;
 
 begin
 
-comb: process(dist,pos1,pos2,estado_luchador1,estado_luchador2)
+sinc:process(clk,reset)
+begin
+	if(reset='1')then
+		fin_juego<='0';
+		vida1 <= 3;
+		vida2 <= 3;
+	elsif(rising_edge(clk)) then
+		vida1 <= corazones1;
+		vida2 <= corazones2;
+		fin_juego<=fin;
+	end if;
+end process;
+
+comb: process(dist,pos1,pos2,estado_luchador1,estado_luchador2,fin,corazones1,corazones2)
 begin
 
-	dist <= pos2 - pos1;
+	dist <= std_logic_vector(unsigned(pos2) - unsigned(pos1));
 
-	if (dist < 80) AND (pos2 > pos1) then
+	if (unsigned(dist) < 20) then
 		if(estado_luchador1 = "01" AND estado_luchador2 = "00") then
-			fin_juego <= '1';
+			if corazones2 > 1 then
+				corazones2 <= corazones2 - 1;
+				if corazones2 = 0 then
+					fin <= '1';
+				else
+					fin <= '0';
+				end if;
+			end if;
 		elsif (estado_luchador1 = "00" AND estado_luchador2 = "01") then
-			fin_juego <= '1';
+			if corazones1 > 1 then
+				corazones1 <= corazones1 - 1;
+				if corazones1 = 0 then
+					fin <= '1';
+				else
+					fin <= '0';
+				end if;
+			end if;
 		else
-			fin_juego <= '0';
+			fin <= '0';
 		end if;
 		
 	else
-		fin_juego <= '0';
+		fin <= '0';
 	end if;
 
 
--- top_level.vhd
+++ top_level.vhd
@@ -1,22 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    19:29:13 05/26/2020 
--- Design Name: 
--- Module Name:    top_level - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -33,11 +14,13 @@ PORT (
 	A: in std_logic;
 	clk: in std_logic;
 	reset: in std_logic;
-	pos1: out integer;
-	pos2: out integer;
-	estado_luchad1: out std_logic_vector(1 downto 0);
-	estado_luchad2: out std_logic_vector(1 downto 0);
-	fin_juego: out std_logic);
+	pos1: out std_logic_vector(9 downto 0);
+	pos2: out std_logic_vector(9 downto 0);
+	estado_luchador1: out std_logic_vector(1 downto 0);
+	estado_luchador2: out std_logic_vector(1 downto 0);
+	fin_juego: out std_logic;
+	vida1 : out integer;
+	vida2 : out integer);
 	
 
 end top_level;
@@ -45,9 +28,8 @@ end top_level;
 architecture Behavioral of top_level is
 
 component estado_luchador
-	Generic (VAL_SAT_CONT:integer:=200;
-		 VAL_SAT_CONT1:integer:=300; --modificar este valor para modificar tiempo que el muñeco salta
-				ANCHO_CONTADOR:integer:=8); -- modificar para que el vector pueda contar hasta numoer de arriba
+		Generic (VAL_SAT_CONT:integer:=200; --modificar este valor para modificar tiempo que el muñeco salta
+				ANCHO_CONTADOR:integer:=20); -- modificar para que el vector pueda contar hasta numoer de arriba
 				
     Port ( clk : in  STD_LOGIC;
            rst : in  STD_LOGIC;
@@ -57,83 +39,43 @@ component estado_luchador
 end component;
 	
 component posicion_luchador
-	Generic ( p_inicial:integer:= 50
-				);
-	Port (  clk : in  STD_LOGIC;
+	Port ( clk : in  STD_LOGIC;
            rst : in  STD_LOGIC;
 			  estado_lucha : in  STD_LOGIC_VECTOR (1 downto 0);
            right : in  STD_LOGIC;
            left : in  STD_LOGIC;
-           posicion : out  integer);
+			  p_inicial: in  STD_LOGIC_VECTOR (9 downto 0);
+           posicion : out  STD_LOGIC_VECTOR (9 downto 0));
 end component;
 
 component estado_juego
-	Port (  pos2 : in  integer;
-           pos1 : in  integer;
+	Port (  clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           pos2 : in  STD_LOGIC_VECTOR (9 downto 0);
+           pos1 : in  STD_LOGIC_VECTOR (9 downto 0);
            estado_luchador1 : in  STD_LOGIC_VECTOR (1 downto 0);
            estado_luchador2 : in  STD_LOGIC_VECTOR (1 downto 0);
+			  vida1 : out integer:= 3;
+			  vida2 : out integer:= 3;
            fin_juego : out  STD_LOGIC);
 end component;
 
 signal estado_lucha1, estado_lucha2 : std_logic_vector(1 downto 0);
-signal p1_aux, p2_aux : integer;
+signal p_inicial_1, p_inicial_2 : std_logic_vector(9 downto 0);
 
 
 begin
 
-	ESTADO_LUCHADOR1 : estado_luchador 
-						generic map( VAL_SAT_CONT => 200, 
-							     VAL_SAT_CONT1=>300,
-										 ANCHO_CONTADOR=> 8)
-										 
-						port map 	(clk => clk, 
-										rst => reset, 
-										up => UP, 
-										down => DOWN, 
-										estado_lucha => estado_lucha1);
-						
-	ESTADO_LUCHADOR2 : estado_luchador 	
-						generic map( VAL_SAT_CONT => 200,
-							     VAL_SAT_CONT1=>300, 
-										 ANCHO_CONTADOR=> 8)
-										 
-						port map (clk => clk, 
-									 rst => reset, 
-									 up => W, 
-									 down => S, 
-									 estado_lucha => estado_lucha2);
-						
-	POSICION_LUCHADOR1 : posicion_luchador 
-						generic map( p_inicial => 50
-						)
-						port map (clk => clk, 
-									 rst => reset, 
-									 right => RIGHT, 
-									 left => LEFT, 
-									 estado_lucha => estado_lucha1, 
-									 posicion => p1_aux);
-									 
-	POSICION_LUCHADOR2 : posicion_luchador 	
-						generic map( p_inicial => 450
-						)
-						port map (clk => clk, 
-									 rst => reset, 
-									 right => D, 
-									 left => A, 
-									 estado_lucha => estado_lucha2,  
-									 posicion => p2_aux);
-									 
-	EST_JUEGO : estado_juego 
-						port map (pos1 => p1_aux, 
-									 pos2 => p2_aux, 
-									 estado_luchador1 => estado_lucha1, 
-									 estado_luchador2 => estado_lucha2, 
-									 fin_juego => fin_juego);
-									
-	pos1<=p1_aux;
-	pos2<=p2_aux;
-	
-	estado_luchad1<= estado_lucha1;
-	estado_luchad2<= estado_lucha2;
+	p_inicial_1 <= std_logic_vector(50);
+	p_inicial_2 <= std_logic_vector(150);
+
+	ESTADO_LUCHADOR1 : estado_luchador 	generic map( VAL_SAT_CONT => 200, ANCHO_CONTADOR=> 20)
+						port map (clk => clk, rst => reset, up => UP, down => DOWN, estado_lucha => estado_lucha1);
+	ESTADO_LUCHADOR2 : estado_luchador 	generic map( VAL_SAT_CONT => 200, ANCHO_CONTADOR=> 20)
+						port map (clk => clk, rst => reset, up => W, down => S, estado_lucha => estado_lucha2);
+	POSICION_LUCHADOR1 : posicion_luchador 	port map (clk => clk, rst => reset, right => RIGHT, left => LEFT, estado_lucha => estado_lucha1, p_inicial => p_inicial_1, posicion => pos1);
+	POSICION_LUCHADOR2 : posicion_luchador 	port map (clk => clk, rst => reset, right => D, left => A, estado_lucha => estado_lucha2, p_inicial => p_inicial_2, posicion => pos2);
+	ESTADO_JUEGO : estado_juego port map (clk => clk, reset => reset, pos1 => pos1, pos2 => pos2, estado_luchador1 => estado_lucha1, estado_luchador2 => estado_lucha2, fin_juego => fin_juego,vida1 => vida1, vida2 => vida2);
 
 end Behavioral;
+
